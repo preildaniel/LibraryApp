90ae284da0225f3b861a597d589198345fa4283f
preildaniel <90619794+preildaniel@users.noreply.github.com>
2025-10-02 13:06:44 +0200

    Feat: style.css, little fixes

--- src/main/java/edu/banki/libraryapp/HelloApplication.java
+++ src/main/java/edu/banki/libraryapp/HelloApplication.java
@@ -1,10 +1,8 @@
 package edu.banki.libraryapp;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-
+import javafx.stage.Stage;import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -12,7 +10,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("createbook-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Könyvtár nyílvántartó");
         stage.setScene(scene);
         stage.show();
     }
