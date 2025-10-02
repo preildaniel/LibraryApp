edeb065e79d160a243bfa7bf6591d908ce324db3
preildaniel <90619794+preildaniel@users.noreply.github.com>
2025-10-02 13:56:49 +0200

    DatamManager implementation

--- src/main/java/edu/banki/libraryapp/model/DataManager.java
+++ src/main/java/edu/banki/libraryapp/model/DataManager.java
@@ -1,4 +1,27 @@
 package edu.banki.libraryapp.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DataManager {
+
+    private static final List<Book> books = new ArrayList<>();
+
+    public static void addBook(Book book) {
+        books.add(book);
+    }
+
+    public static Map<String, Long> getStock() {
+        Map<String, Long> stock = new HashMap<>();
+        //TODO ide jön majd a logic
+
+
+        for (Book book : books) {
+            //stock.put(key, value)
+            stock.put(book.getCategory(), stock.getOrDefault(book.getCategory(), 0L) + 1);
+        }
+        return stock;
+    }
 }
